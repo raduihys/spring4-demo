e13742439dd82652ed15ad1d717abce960fc2abe
黄永升 <huangyongsheng@rd.com>
2016-09-30 10:51:24 +0800

    sadfa

--- src/test/java/com/hys/spring4demo/test/UserServiceTest.java
+++ src/test/java/com/hys/spring4demo/test/UserServiceTest.java
@@ -4,11 +4,14 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hys.spring4demo.service.PeopleService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Configuration
@@ -17,10 +20,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class UserServiceTest {
     @Resource
     UserService userService;
+    @Autowired
+	private PeopleService peopleService;
 
 
     @Test
     public void testBeans() {
-    	System.out.println(userService);
+    	System.out.println(userService+"->"+peopleService);
     }
 }
